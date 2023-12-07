92b80713359456f945b00ae3083f08efbf86e124
azrodex <145688698+azrodex@users.noreply.github.com>
2023-12-07 12:53:57 +0100

    ajout de la vérification

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,28 @@
 console.log("Script bien chargé")
+
+function ConfirmPasswords() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirm-password").value;
+
+    if (password !== confirmPassword) {
+      alert("Les mots de passe ne correspondent pas !");
+      return false;
+    }
+
+    return true;
+}
+
+
+// function validate() { 
+//     var msg; 
+//     var str = document.getElementById("password").value; 
+//     if (str.match( /[0-9]/g) && 
+//             str.match( /[A-Z]/g) && 
+//             str.match(/[a-z]/g) && 
+//             str.match( /[^a-zA-Z\d]/g) &&
+//             str.length >= 10) 
+//         msg = "<p style='color:green'>Mot de passe fort.</p>"; 
+//     else 
+//         msg = "<p style='color:red'>Mot de passe faible.</p>"; 
+//     document.getElementById("msg").innerHTML= msg; 
+// }
--- app/views/page.py
+++ app/views/page.py
@@ -1,22 +1,20 @@
-# Importer les modules Flask nécessaires
-from flask import Blueprint, render_template
+# # Importer les modules Flask nécessaires
+# from flask import Blueprint, render_template
 
-# Créer un objet Blueprint pour les routes liées à la page de contact
-page_bp = Blueprint('page', __name__, url_prefix='/page')
+# page_bp = Blueprint('page', __name__, url_prefix='/page')
 
-# Définir une route pour la page de contact
-@page_bp.route('/contact', methods=['GET'])
-def contact_page():
-    # Afficher la page de contact
-    return render_template('page/contact.html')
+
+# @page_bp.route('/contact', methods=['GET'])
+# def contact_page():
+#     return render_template('page/contact.html')
 
 
 
-@page_bp.route('/sevice', methods=['GET'])
-def service_page():
-    return render_template('page/services.html')
+# @page_bp.route('/sevice', methods=['GET'])
+# def service_page():
+#     return render_template('page/services.html')
 
 
-@page_bp.route('/decouvrir', methods=['GET'])
-def decouvrir_page():
-    return render_template('page/découvrir.html')
+# @page_bp.route('/decouvrir', methods=['GET'])
+# def decouvrir_page():
+#     return render_template('page/découvrir.html')
